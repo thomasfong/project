8b11b861716d3128410320e6e19848028feaf622
Thonas Fong <thomasfong1020@gmail.com>
2025-08-15 06:40:25 +0800

    update

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+document.querySelector('#currentYear').textContent = new Date().getFullYear();
+document.querySelector('#lastModified').textContent = `Last Modified: ${document.lastModified}`;
+
+// Create the date object
+const currentDate = new Date();
+const lastModDate = new Date(document.lastModified);
+
+// Set the inner html text of the elements
+currentYear.innerHTML = currentDate.getFullYear();
+lastModified.innerHTML = `Last Modification: ${lastModDate.getMonth() + 1}/${lastModDate.getDate()}/${lastModDate.getFullYear()} ${lastModDate.getHours()}:${lastModDate.getMinutes()}:${lastModDate.getSeconds()}`;
